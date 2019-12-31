4ad0f2e73b7ed377a218fd4d3972b9cd292d1d12
yusuke kawamura <yusuke.kawamura1964@gmail.com>
2019-12-31 09:52:06 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,18 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
   end
+  
+  def setup
+    @user       = users(:michael)
+  end
+
+  test "layout links when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)    
+    assert_select "a[href=?]", logout_path
+  end
 end
